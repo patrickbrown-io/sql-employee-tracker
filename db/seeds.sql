2ac5a4a153b8515156d3fae4f22b770a8cf91152
Patrick Brown <breepatbree@gmail.com>
2021-09-02 15:36:55 -0600

    Cleaned up db files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,95 +1,32 @@
-USE employees_db;
---  DEPARTMENT
 INSERT INTO
   department (name)
 VALUES
-  ("Sales");
-INSERT INTO
-  department (name)
-VALUES
-  ("Engineering");
-INSERT INTO
-  department (name)
-VALUES
-  ("Finance");
-INSERT INTO
-  department (name)
-VALUES
-  ("Legal");
---  ROLES
+  ('Sales'),
+  ('IT'),
+  ('Customer Service'),
+  ('Engineering'),
+  ('Pricing');
 INSERT INTO
   role (title, salary, department_id)
 VALUES
-  ("Sales Lead", 100000, 1);
--- Sales DEPT
-INSERT INTO
-  role (title, salary, department_id)
-VALUES
-  ("Salesperson", 80000, 1);
--- SALES DEPT
-INSERT INTO
-  role (title, salary, department_id)
-VALUES
-  ("Lead Engineer", 150000, 2);
--- Engineering DEPT
-INSERT INTO
-  role (title, salary, department_id)
-VALUES
-  ("Software Engineer", 120000, 2);
--- ENGINEERING DEPT
-INSERT INTO
-  role (title, salary, department_id)
-VALUES
-  ("Account Manager", 150000, 3);
--- ACCOUNTING DEPT
-INSERT INTO
-  role (title, salary, department_id)
-VALUES
-  ("Accountant", 125000, 3);
--- ACCOUNTING DEPT
-INSERT INTO
-  role (title, salary, department_id)
-VALUES
-  ("Legal Team Lead", 250000, 4);
--- LEGAL DEPT
-  /* === || EMPLOYEE ARRAY || === */
+  ('Standard Salesperson', 50000, 1),
+  ('Manager of Sales', 90000, 1),
+  ('Junior Tech Support', 100000, 2),
+  ('Lead Tech Support', 150000, 2),
+  ('Customer Support Technician', 40000, 3),
+  ('Lead Customer Service', 80000, 3),
+  ('Junior Engineer', 70000, 4),
+  ('Senior Engineer', 120000, 4),
+  ('Pricing Analyst', 60000, 4),
+  ('Lead Pricing Analyst', 70000, 4);
 INSERT INTO
   employee (first_name, last_name, role_id, manager_id)
-  /* Michael Scott - Sales Lead - Sales Department */
 VALUES
-  ("Michael", "Scott", 1, null);
-INSERT INTO
-  employee (first_name, last_name, role_id, manager_id)
-  /* James Halpert - Sales Lead - Sales Department */
-VALUES
-  ("James", "Halpert", 1, 1);
-INSERT INTO
-  employee (first_name, last_name, role_id, manager_id)
-  /* Pam Halpert - Salesperson - Sales Department */
-VALUES
-  ("Pam", "Halpert", 2, 2);
-INSERT INTO
-  employee (first_name, last_name, role_id, manager_id)
-  /* Dwight Schrute - Lead Engineer - Engineering Department */
-VALUES
-  ("Dwight", "Schrute", 3, null);
-INSERT INTO
-  employee (first_name, last_name, role_id, manager_id)
-  /* Ryan Temp - Software Engineer - Engineering Department */
-VALUES
-  ("Ryan", "Temp", 4, 4);
-INSERT INTO
-  employee (first_name, last_name, role_id, manager_id)
-  /* Kelly Kapour - Account Manager- Accounting Department */
-VALUES
-  ("Kelly", "Kapour", 5, null);
-INSERT INTO
-  employee (first_name, last_name, role_id, manager_id)
-  /* Kevin Malone - Accountant - Accounting Department */
-VALUES
-  ("Kevin", "Malone", 6, 6);
-INSERT INTO
-  employee (first_name, last_name, role_id, manager_id)
-  /* Phyllis Vance - Leagl Team Lead - Legal Department */
-VALUES
-  ("Phyllis", "Vance", 7, null);
+  ('Michael', 'Scott', 1, NULL),
+  ('Jim', 'Halpert', 2, 1),
+  ('Dwight', 'Schrute', 3, NULL),
+  ('Stanley', 'Hudson', 4, 3),
+  ('Phylis', 'Vance', 5, NULL),
+  ('Meredith', 'Palmer', 6, 5),
+  ('Kevin', 'Malone', 7, NULL),
+  ('Angela', 'Martin', 8, 7);
--- server.js
+++ server.js
@@ -2,3 +2,20 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 require('console.table')
+
+//  Initial prompt
+
+    // inquirer
+        //  View All Departments
+        //  View All Roles
+        //  View All Employees
+        //  Add A Department
+        //  Add a Role
+        //  Add an employee
+        //  Update an Employee
+
+    //  View All Departments
+        //  show table of department names and ids
+
+    //  View All Roles
+        //  Presented w job title, role id, department the role is 
